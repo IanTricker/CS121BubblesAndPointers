aae9031e0d804aca294b60fcee36daa1c71ee115
Ian Tricker <67514513+IanTricker@users.noreply.github.com>
2025-01-22 17:50:04 +0000

    Made bubble and finnished bubbles.c

--- bubbles.c
+++ bubbles.c
@@ -23,3 +23,32 @@ int main(){
 
   return(0);
 } // end main
+
+void printValues(int* values){
+  int i;
+  printf("[");
+  for(i=0;i<MAX;i++){
+    printf("%d ",values[i]);
+  } // end i for loop
+  printf("]\n");
+} // end printValues
+
+void sort(int* values){
+  printf("start");
+  int i;
+  int j;
+  for(i=0;i<MAX-1;i++){
+    for(j=0;j<MAX-1;j++){
+      if(values[j] > values[j+1]){
+        swap(&values[j], &values[j+1]);
+	printValues(values);
+      } // end if values
+    } // end j for loop
+  } // end i for loop
+} // end sort
+
+void swap(int* x, int* y){
+  *x=*x+*y;
+  *y=*x-*y;
+  *x=*x-*y;
+} // end swap
